9840e6c6321d0a7fdf2d9c1301e3b93922bf5c5f
snakamura <snakamura@8af8166b-12a6-a448-a533-9086ace3f9f6>
2006-02-03 01:12:50 +0000

    Use svn info --xml to avoid locale problems.
    git-svn-id: https://subversion.assembla.com/svn/q3/trunk@2722 8af8166b-12a6-a448-a533-9086ace3f9f6

--- q3/build.sh
+++ q3/build.sh
@@ -206,7 +206,7 @@ installer)
     ;;
 
 *)
-	REVISION=`LC_MESSAGES=C svn info | grep "Last Changed Rev" | sed -e 's/[^0-9]\+//'`
+	REVISION=`svn info --xml | xsltproc --param path "'info/entry/commit/@revision'" xpath.xsl -`
 	if [ -f revision ]; then
 		OLDREVISION=`cat revision`
 	fi
