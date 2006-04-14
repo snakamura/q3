529479187917f383639296957d0a31dbbf50ed3e
snakamura <snakamura@8af8166b-12a6-a448-a533-9086ace3f9f6>
2006-04-14 11:57:40 +0000

    Get latest version of resource from all localized resources.
    git-svn-id: https://subversion.assembla.com/svn/q3/trunk@2961 8af8166b-12a6-a448-a533-9086ace3f9f6

---
+++ q3/qm/version.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# $Id$
+
+getRevision() {
+	LC_MESSAGES=C svn info $1 | grep "Last Changed Rev" | cut -f 4 -d ' '
+}
+
+getNewestPlatformRevision() {
+	rev=`getRevision $1`
+	for p in hpc ppc; do
+		x=`dirname $1`/$p/`basename $1`
+		if [ -f $x ]; then
+			revx=`getRevision $x`
+			if [ $rev -lt $revx ]; then
+				rev=$revx;
+			fi
+		fi
+	done
+	echo $rev
+}
+
+getNewestRevision() {
+	rev=`getNewestPlatformRevision $1`
+	base=`echo $1 | sed -e 's%/.*%%'`
+	rest=`echo $1 | sed -e 's%[^/]*/%%'`
+	for l in $base.*/$rest; do
+		if [ -f $l ]; then
+			revx=`getNewestPlatformRevision $l`
+			if [ $rev -lt $revx ]; then
+				rev=$revx
+			fi
+		fi
+	done
+	echo $rev
+}
