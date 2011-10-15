60c1f5a3f6aa0e488e886d8a868530f527b67937
snakamura <snakamura@8af8166b-12a6-a448-a533-9086ace3f9f6>
2011-10-15 15:51:31 +0000

    Fixed it crashed when turning on HTML mode while showing a message in a folder with FLAG_CACHEWHENREAD.
    git-svn-id: https://subversion.assembla.com/svn/q3/trunk@4409 8af8166b-12a6-a448-a533-9086ace3f9f6

--- q3/qm/src/ui/messagewindow.cpp
+++ q3/qm/src/ui/messagewindow.cpp
@@ -440,8 +440,14 @@ void qm::MessageWindowImpl::modeChanged(const MessageViewModeEvent& event)
 	
 	unsigned int nAdded = event.getAddedMode();
 	unsigned int nRemoved = event.getRemovedMode();
-	if (nAdded & nSetMode || nRemoved & nSetMode)
-		setMessage(pMessageModel_->getCurrentMessage(), false);
+	if (nAdded & nSetMode || nRemoved & nSetMode) {
+		MessageContext *pContext = pMessageModel_->getCurrentMessage();
+		std::auto_ptr<MessageContext> pContextCopy(pContext->safeCopy());
+		if (pContextCopy.get())
+			pContext = pContextCopy.get();
+		
+		setMessage(pContext, false);
+	}
 	
 	if (nAdded & MessageViewMode::MODE_SELECT)
 		pMessageViewWindow_->setSelectMode(true);
